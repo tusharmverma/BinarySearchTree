ac58f0c4e2d85f30096f9ea97dbd850363d0b2f8
Tushar Verma <tushar.m.verma@gmail.com>
2018-09-18 08:40:00 -0500

    Added testing for the Binary Search Tree

--- BST/main.cpp
+++ BST/main.cpp
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+#include "BinarySearchTree.h"
+#include "Node.h"
+
+
+int main()
+{
+    BinarySearchTree t1; //Create a Binary tree
+    t1.insert (333); //Adding a value to the tree
+    t1.insert (888);
+    t1.insert (222);
+    t1.insert (777);
+    t1.display();
+    
+}
 
